7277f2f24a9506670f7bf93652cc7e9145423429
Cyb3rh4ck <efren.hernandez@rjconsultores.com.mx>
2021-09-26 17:02:18 -0500

    path variable

--- src/main/java/com/socialmedia/app/controller/HelloWorldController.java
+++ src/main/java/com/socialmedia/app/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.socialmedia.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.socialmedia.app.bean.HelloWorldBean;
@@ -16,5 +17,10 @@ public class HelloWorldController {
 	@GetMapping("/hello-wordl-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping("/hello-wordl/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean("Hello World " + name);
 	}
 }
