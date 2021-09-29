39d8b4fe49b35670fa168c3e887e83384c33a720
Cyb3rh4ck <efren.hernandez@rjconsultores.com.mx>
2021-09-29 08:10:52 -0500

    Static Filtering

---
+++ src/main/java/com/socialmedia/app/controller/StaticFilterExample.java
@@ -0,0 +1,19 @@
+package com.socialmedia.app.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.socialmedia.app.bean.SomeBean;
+
+@RestController
+public class StaticFilterExample {
+
+	@GetMapping("static-filter")
+	public List<SomeBean> statiFilter() {
+		return Arrays.asList(new SomeBean("value1", "value2", "value3"), 
+				new SomeBean("value2-1","value2-2","value2-3"));
+	}
+}
